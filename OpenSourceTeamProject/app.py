1a630259fd3ae6cb4ba9d9e07baea3ea667c8012
Chanran kim <126753671+isliese@users.noreply.github.com>
2024-06-17 23:56:53 +0900

    Add files via upload
    parkinglot 추가해서 다시 올림

---
+++ OpenSourceTeamProject/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('Home.html')
+
+@app.route('/Disabled')
+def disabled():
+    return render_template('Disabled.html')
+
+@app.route('/Login')
+def login():
+    return render_template('Login.html')
+
+@app.route('/LoginSuccess')
+def loginsuccess():
+    return render_template('LoginSuccess.html')
+
+@app.route('/NoDisabled')
+def nodisabled():
+    return render_template('NoDisabled.html')
+
+@app.route('/ParkingLot')
+def parkinglot():
+    return render_template('ParkingLot.html')
+
+@app.route('/ParkingStateManaging')
+def parkingstatemanaging():
+    return render_template('ParkingStateManaging.html')
+
+@app.route('/SignUp')
+def signup():
+    return render_template('SignUp.html')
+
+@app.route('/SignUpSuccess')
+def signupsuccess():
+    return render_template('SignUpSuccess.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
