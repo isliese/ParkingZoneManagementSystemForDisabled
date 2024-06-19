7aa54147e0160a4e6c4236efd0b4f9167e7af2da
Chanran kim <126753671+isliese@users.noreply.github.com>
2024-06-19 14:19:40 +0900

    Add files via upload

---
+++ OpenSourceTeamProject/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('Home.html')
+
+@app.route('/Disabled')
+def disabled():
+    return render_template('Disabled.html')
+
+@app.route('/Login')
+def login():
+    return render_template('Login.html')
+
+@app.route('/LoginSuccess')
+def loginsuccess():
+    return render_template('LoginSuccess.html')
+
+@app.route('/NoDisabled')
+def nodisabled():
+    return render_template('NoDisabled.html')
+
+@app.route('/ParkingLot')
+def parkinglot():
+    return render_template('ParkingLot.html')
+
+@app.route('/ParkingStateManaging')
+def parkingstatemanaging():
+    return render_template('ParkingStateManaging.html')
+
+@app.route('/SignUp')
+def signup():
+    return render_template('SignUp.html')
+
+@app.route('/SignUpSuccess')
+def signupsuccess():
+    return render_template('SignUpSuccess.html')
+
+@app.route('/Report')
+def report():
+    return render_template('Report.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
